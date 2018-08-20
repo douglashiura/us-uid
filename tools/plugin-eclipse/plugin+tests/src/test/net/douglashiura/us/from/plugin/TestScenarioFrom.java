ede47980378a453d54f7de0c556b1f270f0fdc51
Douglas Hiura Longo <douglashiura@gmail.com>
2018-08-20 20:37:47 -0300

    Adding Eclipse plugin's tests

---
+++ tools/plugin-eclipse/plugin+tests/src/test/net/douglashiura/us/from/plugin/TestScenarioFrom.java
@@ -0,0 +1,126 @@
+package test.net.douglashiura.us.from.plugin;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import net.douglashiura.usuid.plugin.type.Input;
+import net.douglashiura.usuid.plugin.type.Interaction;
+import net.douglashiura.usuid.plugin.type.Output;
+import net.douglashiura.usuid.plugin.type.Scenario;
+
+public class TestScenarioFrom {
+	private String interacaoInicial = "{\"type\": \"br.ufsc.leb.uid.scenario.Interacao\","
+			+ "\"id\": \"8c9987b0-3189-1087-2aba-9df20814018b\"," + "\"x\": 638," + "\"y\": 291," + "\"width\": 93,"
+			+ "\"height\": 80," + "\"alpha\": 1," + "\"userData\": {" + "\"result\": \"\","
+			+ "\"model\": \"Fixture8Puzzle\"" + "}," + "\"cssClass\": \"br_ufsc_leb_uid_scenario_Interacao\","
+			+ "\"ports\": [" + "{" + "\"name\": \"output0\"," + "\"port\": \"draw2d.OutputPort\","
+			+ "\"locator\": \"br.ufsc.leb.uid.scenario.ResourceLocator\"" + "}" + "]," + "\"bgColor\": \"#FFFFFF\","
+			+ "\"color\": \"#000000\"," + "\"stroke\": 1," + "\"radius\": 500" + "}";
+
+	private String interacaoAlvo = "{\"type\": \"br.ufsc.leb.uid.scenario.Interacao\",\"id\": \"6063b665-b963-62dd-de91-195d0d6791ad\","
+			+ "\"x\": 626,\"y\": 156,\"width\": 113,\"height\": 83,\"alpha\": 1,\"userData\": {\"result\":\"\",\"model\": \"Alvo\"},"
+			+ "\"cssClass\": \"br_ufsc_leb_uid_scenario_Interacao\",\"ports\": [{\"name\": \"input0\","
+			+ "\"port\": \"draw2d.InputPort\",\"locator\": \"br.ufsc.leb.uid.scenario.TargetLocator\"}],\"bgColor\": \"#FFFFFF\","
+			+ "\"color\": \"#000000\",\"stroke\": 1,\"radius\": 500 }";
+
+	private String transaction = "{\"type\": \"br.ufsc.leb.uid.scenario.Transaction\","
+			+ "\"id\": \"dcc6fc42-5fb4-d204-0d39-fbaf82c1cf85\"," + "\"alpha\": 1,\"userData\": {" + "\"result\": \"\","
+			+ "\"model\": \"\" }," + "\"cssClass\": \"br_ufsc_leb_uid_scenario_Transaction\","
+			+ "\"stroke\": 1,\"color\": \"#000000\",\"outlineStroke\": 0," + "\"outlineColor\": \"none\","
+			+ "\"policy\": \"draw2d.policy.line.LineSelectionFeedbackPolicy\","
+			+ "\"router\": \"draw2d.layout.connection.DirectRouter\"," + "\"radius\": 2,\"source\": {"
+			+ "\"node\":\"8c9987b0-3189-1087-2aba-9df20814018b\"," + "\"port\": \"output0\" },\"target\": {"
+			+ "\"node\": \"6063b665-b963-62dd-de91-195d0d6791ad\",\"port\":\"input0\"" + "}}";
+
+	private String entrada = "{\"type\": \"br.ufsc.leb.uid.scenario.UserInput\",\"id\": \"deb51e97-bd3f-82c3-74d2-ef1815a42265\","
+			+ "\"x\": 654,\"y\": 205,\"width\": 43,\"height\": 23,\"alpha\": 1,\"userData\": {\"result\": \"\",\"model\": \"entrada\""
+			+ "},\"cssClass\": \"br_ufsc_leb_uid_scenario_UserInput\",\"composite\": \"6063b665-b963-62dd-de91-195d0d6791ad\","
+			+ "\"ports\": [],\"bgColor\": \"none\",\"color\": \"#000000\",\"stroke\": 1,\"radius\": 0,"
+			+ "\"text\": \"Valor\", \"outlineStroke\": 0,\"outlineColor\": \"none\",\"fontSize\": 12,"
+			+ "\"fontColor\": \"#080808\",\"fontFamily\": null }";
+
+	private String saida = "{\"type\": \"br.ufsc.leb.uid.scenario.SystemOutput\",\"id\": \"a2df2c45-be51-27cc-ae8d-f2772ceaa6f4\","
+			+ "\"x\": 647,\"y\": 170,\"width\": 43,\"height\": 23, \"alpha\": 1,\"userData\": {\"result\": \"\","
+			+ "\"model\": \"saida\"},\"cssClass\": \"br_ufsc_leb_uid_scenario_SystemOutput\",\"composite\": \"6063b665-b963-62dd-de91-195d0d6791ad\","
+			+ "\"ports\": [],\"bgColor\": \"none\",\"color\": \"#FFFFFF\",\"stroke\": 1,\"radius\": 0,"
+			+ "\"text\": \"Valor\",\"outlineStroke\": 0,\"outlineColor\": \"none\",\"fontSize\": 12,\"fontColor\": \"#000000\","
+			+ "\"fontFamily\": null}";
+
+	@Test
+	public void interacaoComSaidaSistema() throws Exception {
+		Interaction interacao = new Scenario(String.format("[%s,%s]", interacaoAlvo, saida)).firstState();
+		List<Output> outputs = interacao.getOutputs();
+		Output output = outputs.get(0);
+		assertEquals(626, interacao.getGeometry().getX().intValue());
+		assertEquals(156, interacao.getGeometry().getY().intValue());
+		assertEquals(113, interacao.getGeometry().getWidth().intValue());
+		assertEquals(83, interacao.getGeometry().getHeight().intValue());
+		assertEquals("Alvo", interacao.getFixture());
+		assertEquals("6063b665-b963-62dd-de91-195d0d6791ad", interacao.getId());
+		assertNull(interacao.getTransaction());
+
+		assertEquals(647, output.getGeometry().getX().intValue());
+		assertEquals(170, output.getGeometry().getY().intValue());
+		assertEquals(43, output.getGeometry().getWidth().intValue());
+		assertEquals(23, output.getGeometry().getHeight().intValue());
+		assertEquals("a2df2c45-be51-27cc-ae8d-f2772ceaa6f4", output.getId());
+		assertEquals("saida", output.getFixture());
+		assertEquals("Valor", output.getValue());
+
+	}
+
+	@Test
+	public void interacaoAlvo() throws Exception {
+		Interaction interacao = new Scenario(String.format("[%s]", interacaoInicial)).firstState();
+		assertEquals(638, interacao.getGeometry().getX().intValue());
+		assertEquals(291, interacao.getGeometry().getY().intValue());
+		assertEquals(93, interacao.getGeometry().getWidth().intValue());
+		assertEquals(80, interacao.getGeometry().getHeight().intValue());
+		assertEquals("Fixture8Puzzle", interacao.getFixture());
+		assertNull(interacao.getTransaction());
+	}
+
+	@Test
+	public void interacaoComEntradaDeUsuario() throws Exception {
+		Interaction interacao = new Scenario(String.format("[%s,%s]", interacaoAlvo, entrada)).firstState();
+		List<Input> inputs = interacao.getInputs();
+		Input input = inputs.get(0);
+		assertEquals(626, interacao.getGeometry().getX().intValue());
+		assertEquals(156, interacao.getGeometry().getY().intValue());
+		assertEquals(113, interacao.getGeometry().getWidth().intValue());
+		assertEquals(83, interacao.getGeometry().getHeight().intValue());
+		assertEquals("Alvo", interacao.getFixture());
+		assertNull(interacao.getTransaction());
+		assertEquals(654, input.getGeometry().getX().intValue());
+		assertEquals(205, input.getGeometry().getY().intValue());
+		assertEquals(43, input.getGeometry().getWidth().intValue());
+		assertEquals(23, input.getGeometry().getHeight().intValue());
+		assertEquals("entrada", input.getFixture());
+		assertEquals("deb51e97-bd3f-82c3-74d2-ef1815a42265", input.getId());
+		assertEquals("Valor", input.getValue());
+	}
+
+	@Test
+	public void interacaoSaidaEEntrada() throws Exception {
+		Interaction interacao = new Scenario(String.format("[%s,%s,%s]", transaction, interacaoInicial, interacaoAlvo))
+				.firstState();
+		Interaction target = interacao.getTransaction().getTarget();
+		assertNull(target.getTransaction());
+		assertEquals("dcc6fc42-5fb4-d204-0d39-fbaf82c1cf85", interacao.getTransaction().getId());
+		assertEquals(638, interacao.getGeometry().getX().intValue());
+		assertEquals(291, interacao.getGeometry().getY().intValue());
+		assertEquals(93, interacao.getGeometry().getWidth().intValue());
+		assertEquals(80, interacao.getGeometry().getHeight().intValue());
+		assertEquals("Fixture8Puzzle", interacao.getFixture());
+		assertEquals(626, target.getGeometry().getX().intValue());
+		assertEquals(156, target.getGeometry().getY().intValue());
+		assertEquals(113, target.getGeometry().getWidth().intValue());
+		assertEquals(83, target.getGeometry().getHeight().intValue());
+		assertEquals("Alvo", target.getFixture());
+	}
+
+}
---
+++ tools/plugin-eclipse/plugin+tests/src/test/net/douglashiura/us/from/plugin/TesteCamelCase.java
@@ -0,0 +1,43 @@
+package test.net.douglashiura.us.from.plugin;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import net.douglashiura.us.CamelCase;
+
+public class TesteCamelCase {
+
+	@Test
+	public void camel() throws Exception {
+		assertEquals("Model", CamelCase.transform("model"));
+		assertEquals("", CamelCase.transform(null));
+		assertEquals("", CamelCase.transform(""));
+		assertEquals("M", CamelCase.transform("m"));
+	}
+
+	@Test
+	public void to() throws Exception {
+		assertEquals("toModel", CamelCase.to("model"));
+		assertEquals("to", CamelCase.to(null));
+		assertEquals("to", CamelCase.to(""));
+		assertEquals("toM", CamelCase.to("m"));
+	}
+
+	@Test
+	public void get() throws Exception {
+		assertEquals("getModel", CamelCase.get("model"));
+		assertEquals("get", CamelCase.get(null));
+		assertEquals("get", CamelCase.get(""));
+		assertEquals("getM", CamelCase.get("m"));
+	}
+
+	@Test
+	public void set() throws Exception {
+		assertEquals("setModel", CamelCase.set("model"));
+		assertEquals("set", CamelCase.set(null));
+		assertEquals("set", CamelCase.set(""));
+		assertEquals("setM", CamelCase.set("m"));
+	}
+
+}
